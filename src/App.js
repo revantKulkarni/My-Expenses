0a37a4d49deb2e2dd823acfc123265ea0a1b10a6
Revant Kulkarni <revanth.skulkarni@protonmail.com>
2021-07-17 23:41:25 +0530

    Fixed id in App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import ExpenseItem from './components/Expenses';
 
 function App() {
   const expenses = [
-    { id: "e1", title: 'Car Insurance', amount: 194.67, date: new Date(2021, 2, 10) },
-    { id: "e2", title: 'Bike Insurance', amount: 29.67, date: new Date(2021, 1, 20) },
-    { id: "e3", title: 'Health Insurance', amount: 500.17, date: new Date(2021, 3, 10) },
+    { id: "1", title: 'Car Insurance', amount: 194.67, date: new Date(2021, 2, 10) },
+    { id: "2", title: 'Bike Insurance', amount: 29.67, date: new Date(2021, 1, 20) },
+    { id: "3", title: 'Health Insurance', amount: 500.17, date: new Date(2021, 3, 10) },
   ];
   return (
     <div>
@@ -12,5 +12,4 @@ function App() {
     </div>
   )
 }
-
 export default App;
